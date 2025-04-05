dcdf64751d56ef37418d5ee8ae6dde91995d86b4
Bryan Reamillo <bryanreamillo2403@gmail.com>
2025-04-05 15:53:01 +0800

    Refactor pixel creation and reset functionality; enhance canvas interaction with color selection

--- index.js
+++ index.js
@@ -6,27 +6,50 @@ document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.querySelector(".grid");
     const gridBtn = document.querySelector(".controls--grid-button");
     const field = document.querySelector(".controls__field");
+    const resetBtn = document.querySelector(".controls--reset-canvas");
+    const colorBtn = document.querySelector(".controls--choose-color");
+    let pixel = document.createElement("div");
+    
 
     //setting canvas 
-    canvas.setAttribute("style", "width: 800px; height: 800px; border: 3px dashed blue; margin-top: auto; margin-bottom: auto; display: flex; flex-wrap: nowrap;");
+    canvas.setAttribute("style", "width: 800px; height: 800px; border: 3px dashed blue; margin-top: auto; margin-bottom: auto; display: flex; flex-wrap: wrap;");
 
     //variables for canvas pixels;
     let rows = 0;
     let columns = 0;
     let inputValue = undefined;
-    let pixelW = 0;
-    let pixelL = 0;
-    let GRID_BOARD = 800;
+    let color = 'black';
+
+    
 
     //Functions
-    let createPixels = (grid_board, input, numOfPixels) => {
-        pixelDimension = grid_board / input;
+    let createPixels = (input, numOfPixels) => {
+
+        if (canvas.hasChildNodes()) {
+            while (canvas.firstChild) {
+                canvas.removeChild(canvas.firstChild);
+            }
+        }
+
         for (let i = 0; i < numOfPixels; i++) {
-            let pixel = document.createElement("div");
-            pixel.setAttribute("style", `width: ${pixelDimension}px; height: ${pixelDimension}px; border: 1px solid black;`);
+            pixel = document.createElement("div");
+            pixel.classList = 'pixel';
+            pixel.setAttribute("style", `flex-basis: calc(100% / ${input}); border: 1px solid black;`);
             canvas.appendChild(pixel);
         }
     }
+
+    let resetCanvas = () => {
+        if (canvas.hasChildNodes()) {
+            while (canvas.firstChild) {
+                canvas.removeChild(canvas.firstChild);
+            }
+        }
+        else {
+            alert("Empty Canvas");
+        }
+    }
+    
 
     //Listeners
     field.addEventListener('input', (e) => {
@@ -58,19 +81,41 @@ document.addEventListener('DOMContentLoaded', () => {
             } else if (isNaN(convertedValue)) {
                 throw "NaN! Just Ignore, Debugging Purposes"
             }
+
             inputGridSize = convertedValue;
             rows = convertedValue;
             columns = convertedValue;
             canvasArea = rows * columns;
             console.log(`Creating a canvas size by: ${canvasArea} squares`);
 
-            createPixels(GRID_BOARD, inputGridSize, canvasArea);
+            createPixels(inputGridSize, canvasArea);
                
         } catch (error) {
             console.error(error);
         }
     });
 
+
+    resetBtn.addEventListener('click', () => {
+        console.log("Resetting Canvas");
+        resetCanvas();
+    });
+
+    colorBtn.addEventListener('input', (e) => {
+        color = e.target.value;
+    });
+
+    //Highlight pixel if alt key was pressed
+    canvas.addEventListener('mouseover', (e) => {
+        if (e.target.classList.contains('pixel')) {
+            if (e.altKey) {
+                e.target.style.backgroundColor = color;
+            } else {
+                console.log("Press alt key to draw");
+            }
+        }
+    });
+    
 
 
 
